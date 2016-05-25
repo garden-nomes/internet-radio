3dd5c64bba3da1166e5fe46182e8b47e62dc50fb
thebareju <noahbweiner@gmail.com>
2016-05-25 11:42:00 -0700

    Build rought outline; v buggy

--- server.c
+++ server.c
@@ -9,17 +9,37 @@
 #include <netinet/in.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <pthread.h>
+#include <time.h>
+
+#define MAX_CLIENTS 256
+#define BUF_SIZE 1024
+#define SAMPLE_SIZE 96000
+
+void *sender_thread(void *);
+void *listener_thread(void *);
 
 char *filename;
+int server_fd;
+int done = 0;
 
-int write_file(int, int);   /* from serverlib.c */
-int handle_client(int);
+/* linked list of clients */
+struct client {
+    int fd;
+    struct client *next;
+};
+
+struct client *remove_client(struct client *c);
+struct client *add_client(int fd);
+void destroy_clients(void);
+
+struct client *c_head = NULL;
+pthread_mutex_t client_list_lock = PTHREAD_MUTEX_INITIALIZER;
+
 
 int main(int argc, char *argv[]) {
-    int fd, client_fd, port, len_cli_addr;
-    struct sockaddr_in serv_addr, cli_addr;
-    const char *msg = "Hello from across teh interwebz!";
-    pid_t pid;
+    int port, error;
+    pthread_t listener, sender;
 
     /* usage */
 
@@ -30,13 +50,38 @@ int main(int argc, char *argv[]) {
     port = atoi(argv[1]);
     filename = argv[2];
 
+    /* start threads */
+
+    if ((error = pthread_create(&listener, NULL, listener_thread, &port))
+            || (error = pthread_create(&sender, NULL, sender_thread, NULL)))
+        fprintf(stderr, "Failed to create thread: %s\n", strerror(error));
+
+    if ((error = pthread_join(sender, NULL))
+            || (error = pthread_join(listener, NULL)))
+        fprintf(stderr, "Failed to join thread: %s\n", strerror(error));
+
+    /* clean up */
+
+    destroy_clients();
+    close(server_fd);
+    pthread_mutex_destroy(&client_list_lock);
+    return 0;
+}
+
+void *listener_thread(void *data) {
+    struct client *c;
+    int server_fd, client_fd, len_cli_addr, port;
+    struct sockaddr_in cli_addr, serv_addr;
+
+    port = *((int *)data);
+
     /* initialize networking */
 
     signal(SIGPIPE, SIG_IGN);
 
-    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("Error creating socket");
-        return 1;
+        return NULL;
     }
 
     bzero((char *)&serv_addr, sizeof(serv_addr));
@@ -44,60 +89,141 @@ int main(int argc, char *argv[]) {
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(port);
 
-    if (bind(fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
+    if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))
+            == -1) {
         perror("Error on binding");
-        return 1;
+        return NULL;
     }
 
-    if (listen(fd, 8) == -1) {
+    if (listen(server_fd, 5) == -1) {
         perror("Error on listen");
-        return 1;
+        return NULL;
     }
 
     len_cli_addr = sizeof(cli_addr);
 
-    while (1) {
+    while (!done) {
+
         /* accept client connection */
-        client_fd = accept(fd, (struct sockaddr *)&cli_addr,
+
+        client_fd = accept(server_fd, (struct sockaddr *)&cli_addr,
             (socklen_t *)&len_cli_addr);
         if (client_fd == -1) {
             perror("Error on accept");
-            return 1;
+            return NULL;
         }
 
-        /* start client handler */
+        fprintf(stderr, "Client received\n");
 
-        if ((pid = fork()) == -1)
-            perror("Error on fork");
-        else if (pid > 0) { /* parent */
-            printf("[%ld] client process started\n", (long)pid);
-            close(client_fd);
-        } else { /* child */
-            close(fd);
-            handle_client(client_fd);
+        /* add to clients list */
 
-            printf("[%ld] client process ended\n", (long)getpid());
-            close(client_fd);
-            return 0;
-        }
+        pthread_mutex_lock(&client_list_lock);
+        add_client(client_fd);
+        pthread_mutex_unlock(&client_list_lock);
     }
 
-    /* clean up */
-
-    close(fd);
-    return 0;
+    return NULL;
 }
 
-int handle_client(int fd) {
+void *sender_thread(void *data) {
     int file_fd, n;
+    char *p_buffer;
+    char buffer[BUF_SIZE];
+    int bytes_read, bytes_written, bytes_total = 0;
+    struct client *c;
+    struct timespec sleep_time;
+
+    sleep_time.tv_nsec = (BUF_SIZE / SAMPLE_SIZE) * 1000;
 
     if ((file_fd = open(filename, O_RDONLY)) == -1) {
         perror("Unable to open file");
-        return -1;
+        return NULL;
     }
 
-    if ((n = write_file(file_fd, fd)) == -1)
-        return -1;
+    while (1) {
+        if (c_head) {
 
-    return 0;
+            /* read chunk from file */
+
+            while ((bytes_read = read(file_fd, buffer, BUF_SIZE)) == -1
+                    && errno == EINTR);
+            if (bytes_read == -1) { /* real error */
+                perror("Error on reading from file");
+                return NULL;
+            }
+            if (bytes_read == 0)    /* EOF */
+                break;
+
+            /* write chunk to each client */
+
+            pthread_mutex_lock(&client_list_lock);
+            for (c = c_head; c != NULL; c = c->next) {
+                p_buffer = buffer;
+
+                while (bytes_read > 0) {
+                    while ((bytes_written = write(c->fd, p_buffer, bytes_read)) == -1
+                            && errno == EINTR);
+                    if (bytes_written == -1) {
+                        fprintf(stderr, "Error on writing to client\n");
+                        c = remove_client(c);
+                        break;
+                    }
+                    if (bytes_written == 0)
+                        break;
+
+                    p_buffer += bytes_written;
+                    bytes_read -= bytes_written;
+                }
+            }
+            pthread_mutex_unlock(&client_list_lock);
+        }
+
+        nanosleep(&sleep_time, NULL);
+    }
+
+    done = 1;
+    return NULL;
+}
+
+struct client *remove_client(struct client *c) {
+    struct client *x;
+
+    if (c == c_head)
+        c_head = c->next;
+    else {
+        for (x = c_head; x->next != c; x = x->next);  /* find previous node */
+        x->next = c->next;
+    }
+
+    close(c->fd);
+    free(c);
+    return x;
+}
+
+struct client *add_client(int fd) {
+    struct client *c, *d;
+    c = malloc(sizeof(struct client));
+    c->fd = fd;
+    c->next = NULL;
+
+    if (c_head == NULL)
+        c_head = c;
+    else {
+        d = c_head;
+        for (; d->next != NULL; d = d->next);
+        d->next = c;
+    }
+
+    return c;
+}
+
+void destroy_clients(void) {
+    struct client *c = c_head, *d;
+
+    while (c != NULL) {
+        d = c->next;
+        close(c->fd);
+        free(c);
+        c = d;
+    }
 }
